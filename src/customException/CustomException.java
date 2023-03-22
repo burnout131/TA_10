bd28d3aad0531809bff46f1c5b2e6b2feb90120d
burnout131 <marcrg07@gmail.com>
2023-03-22 20:45:33 +0100

    Cambios en el package ejercicio2 y customException

---
+++ src/customException/CustomException.java
@@ -0,0 +1,9 @@
+package customException;
+
+public class CustomException extends Exception {
+
+	public CustomException(String message) {
+		super(message);
+	}
+
+}
--- src/ejercicio2/MainApp.java
+++ src/ejercicio2/MainApp.java
@@ -1,9 +1,18 @@
 package ejercicio2;
 
+import customException.CustomException;
+
 public class MainApp {
 
 	public static void main(String[] args) {
-		
+		try {
+			System.out.println("Mensaje mostrado por pantalla");
+			throw new CustomException("ESTO ES UNA EXCEPTION");
+		} catch (CustomException e) {
+			System.out.println("Excepcion capturada con mensaje: " + e.getMessage());
+		} finally {
+			System.out.println("Programa terminado");
+		}
 	}
 
 }
